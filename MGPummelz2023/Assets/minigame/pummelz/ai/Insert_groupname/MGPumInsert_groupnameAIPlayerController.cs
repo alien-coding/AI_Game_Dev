560d37655b0a74eee6f5e7875598142d865c9de2
alien-coding <96201379+alien-coding@users.noreply.github.com>
2023-05-11 10:48:21 +0200

    Created AI main classes

---
+++ MGPummelz2023/Assets/minigame/pummelz/ai/Insert_groupname/MGPumInsert_groupnameAIPlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mg.pummelz.Insert_groupname
+{
+    public class MGPumInsert_groupnameAIPlayerController : MGPumStudentAIPlayerController
+    {
+        public const string type = "Insert_groupname";
+
+        public MGPumInsert_groupnameAIPlayerController(int playerID) : base(playerID)
+        {
+            
+        }
+
+        
+        protected override int[] getTeamMartikels()
+        {
+            return new int[] { 1269263, 22222222 };
+        }
+
+        
+        internal override MGPumCommand calculateCommand()
+        {
+            return new MGPumEndTurnCommand(this.playerID);
+        }
+    }
+}
+
