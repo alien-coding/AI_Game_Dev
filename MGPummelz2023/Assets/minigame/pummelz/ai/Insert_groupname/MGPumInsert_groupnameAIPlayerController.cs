5cac81a945fe2e4dd1c6eac3df562acd908983f6
alien-coding <96201379+alien-coding@users.noreply.github.com>
2023-05-12 17:08:22 +0200

    range attacks and multiple steps now possible

--- MGPummelz2023/Assets/minigame/pummelz/ai/Insert_groupname/MGPumInsert_groupnameAIPlayerController.cs
+++ MGPummelz2023/Assets/minigame/pummelz/ai/Insert_groupname/MGPumInsert_groupnameAIPlayerController.cs
@@ -26,23 +26,31 @@ namespace mg.pummelz.Insert_groupname
             {
                 if(unit.ownerID == this.playerID)
                 {
-                    List<MGPumMoveCommand> allMoves = this.getAllMovesForUnit(unit);
-                    List<MGPumAttackCommand> allAttacks = this.getAllAttacksForUnit(unit);
-                    int attackOrMove = this.rng.Next(2); // number is < smaller than maxValue
-
-                    if (attackOrMove == 0 && allMoves.Count > 0)
+                    List<MGPumCommand> allMoves = getAllPossibleMoves(unit);
+                    //Debug.Log(allMoves);
+                    if(allMoves != null && allMoves.Count > 0)
                     {
-                        int moveIndex = this.rng.Next(allMoves.Count);   //number is < than maxValue, therefore do not sutract 1
-                        return allMoves[moveIndex];
-                    }
-                    else if (attackOrMove == 1 && allAttacks.Count > 0)
-                    {
-                        int attackIndex = this.rng.Next(allAttacks.Count);   //number is < than maxValue, therefore do not sutract 1
-                        return allAttacks[attackIndex];
+                        return allMoves[rng.Next(allMoves.Count)];
                     }
                 }
             }
             return new MGPumEndTurnCommand(this.playerID);
+        }
+
+        private List<MGPumCommand> getAllPossibleMoves(MGPumUnit unit)
+        {
+            List<MGPumCommand> allPossibleMoves = new();
+            allPossibleMoves.AddRange(getAllMovesForUnit(unit));
+            allPossibleMoves.AddRange(getAllAttacksForUnit(unit));
+
+            if (allPossibleMoves.Count > 0)
+            {
+                return allPossibleMoves;
+            }
+            else
+            {
+                return null;
+            }
         }
 
 
@@ -53,9 +61,10 @@ namespace mg.pummelz.Insert_groupname
             {
                 Vector2Int position = unit.field.coords;
 
-                foreach (Vector2Int direction in this.getDirections())
+                List<Vector2Int> allVectors = getDirectionsWithDepth(unit.currentSpeed);
+                foreach (Vector2Int vectorToDestination in allVectors)
                 {
-                    Vector2Int movingWay = position + direction;
+                    Vector2Int movingWay = position + vectorToDestination;
                     MGPumField destination = state.getField(movingWay);
 
                     if (destination != null && destination.isEmpty())
@@ -63,14 +72,29 @@ namespace mg.pummelz.Insert_groupname
                         MGPumMoveChainMatcher chainMatcher = unit.getMoveMatcher();
                         MGPumFieldChain chain = new(playerID, chainMatcher);
                         chain.add(state.getField(position));
-                        if (chain.canAdd(destination))
+                       
+                        List<Vector2Int> splittedSteps = splitVectorInSteps(position, movingWay);
+                        foreach (Vector2Int stepVector in splittedSteps)
                         {
-                            chain.add(destination);
+                            //Debug.Log(stepVector + " " + unit.currentSpeed);
+                            MGPumField fieldOfVector = state.getField(stepVector);
+                            if (chain.canAdd(fieldOfVector))
+                            {
+                                chain.add(fieldOfVector);
+                            }
+                            else
+                            {
+                                break;
+                            }
                         }
-                        if (chain.isValidChain())
+                        if (chain.isValidChain() && chain.getLast() == destination)
                         {
                             MGPumMoveCommand move = new(this.playerID, chain, unit);
                             allMoves.Add(move);
+                        }
+                        else
+                        {
+                            Debug.Log("Found unvalid move");
                         }
                         
                     }
@@ -87,9 +111,9 @@ namespace mg.pummelz.Insert_groupname
             {
                 Vector2Int position = unit.field.coords;
 
-                foreach (Vector2Int direction in this.getDirections())
+                foreach (Vector2Int vectorToDestination in this.getDirectionsWithDepth(unit.currentRange))
                 {
-                    Vector2Int attackingWay = position + direction;
+                    Vector2Int attackingWay = position + vectorToDestination;
                     MGPumField attackHere = state.getField(attackingWay);
 
                     if (attackHere != null && !attackHere.isEmpty() && state.getUnitForField(attackHere).ownerID != this.playerID)
@@ -97,19 +121,93 @@ namespace mg.pummelz.Insert_groupname
                         MGPumAttackChainMatcher attackMatcher = unit.getAttackMatcher();
                         MGPumFieldChain chain = new(this.playerID, attackMatcher);
                         chain.add(state.getField(position));
-                        if (chain.canAdd(attackHere))
+
+                        List<Vector2Int> splittedSteps = splitVectorInSteps(position, attackingWay);
+                        foreach (Vector2Int stepVector in splittedSteps)
                         {
-                            chain.add(attackHere);
+                            MGPumField fieldOfVector = state.getField(stepVector);
+                            if (chain.canAdd(fieldOfVector))
+                            {
+                                chain.add(fieldOfVector);
+                            }
+                            else
+                            {
+                                break;
+                            }
                         }
-                        if (chain.isValidChain())
+
+                        
+                        if (chain.isValidChain() && chain.getLast() == attackHere)
                         {
                             MGPumAttackCommand attack = new(this.playerID, chain, unit);
                             allAttacks.Add(attack);
+                        }
+                        else
+                        {
+                            Debug.Log("Found unvalid attack");
                         }
                     }
                 }
             }
             return allAttacks;
+        }
+
+        //returns list of Vectors for surounding, with given depth.
+        //Also contains zero vector
+        private List<Vector2Int> getDirectionsWithDepth(int depth)
+        {
+            List<Vector2Int> directions = new List<Vector2Int>();
+            for (int i = -depth; i <= depth; i++)
+            {
+                for (int j = -depth; j <= depth; j++)
+                {
+                    directions.Add(new Vector2Int(i, j));
+                }
+            }
+            
+            return directions;
+        }
+
+        private List<Vector2Int> splitVectorInSteps(Vector2Int position, Vector2Int destination)
+        {
+            List<Vector2Int> directions = new List<Vector2Int>();
+            while(position != destination)
+            {
+                if(position.x < destination.x && position.y < destination.y) //top right
+                {
+                    position += new Vector2Int(1, 1);
+                }
+                else if (position.x < destination.x && position.y == destination.y)  //right 
+                {
+                    position += new Vector2Int(1, 0);
+                }
+                else if (position.x < destination.x && position.y > destination.y)  //bottom right 
+                {
+                    position += new Vector2Int(1, -1);
+                }
+                else if (position.x == destination.x && position.y > destination.y)  //bottom 
+                {
+                    position += new Vector2Int(0, -1);
+                }
+                else if (position.x > destination.x && position.y > destination.y)  //bottom left
+                {
+                    position += new Vector2Int(-1, -1);
+                }
+                else if (position.x > destination.x && position.y == destination.y)  //left 
+                {
+                    position += new Vector2Int(-1, 0);
+                }
+                else if (position.x > destination.x && position.y < destination.y)  //top left 
+                {
+                    position += new Vector2Int(-1, 1);
+                }
+                else if (position.x == destination.x && position.y < destination.y)  //top 
+                {
+                    position += new Vector2Int(0, 1);
+                }
+                directions.Add(position);
+            }
+            return directions;
         }
     }
 }
